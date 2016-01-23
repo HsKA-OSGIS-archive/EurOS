d196db0c70d1b66eb62bb1ba0e773d099da55fcf
daviiidet <Damones2@topo.upv.es>
2016-01-23 18:47:34 +0100

    1

---
+++ creatabla.sql
@@ -0,0 +1,20 @@
+﻿-- Table: public.hska_exp
+
+-- DROP TABLE public.hska_exp;
+
+CREATE TABLE public.hska_exp
+(
+  "ID" integer,
+  "Name" text,
+  "Lon" numeric,
+  "Lat" numeric,
+  "Height" numeric,
+  "Date" text,
+  "Hour" text,
+  "ODL" numeric
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.hska_exp
+  OWNER TO postgres;
